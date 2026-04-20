52376bf8a53a544a338127774431143c11eb92a9
Abir-Shaikh <shaikhabir044@gmail.com>
2026-04-20 20:33:09 +0530

    structure

---
+++ src/main/java/com/parotha_cloud/ParothaCloudApplication.java
@@ -0,0 +1,13 @@
+package com.parotha_cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParothaCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParothaCloudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/parotha_cloud/ParothaCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parotha_cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParothaCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
