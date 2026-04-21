c961f53f0a5e143f9066d3acd3c42ac452adf32a
Abir-Shaikh <shaikhabir044@gmail.com>
2026-04-21 09:31:55 +0530

    test for homecontroller

--- src/test/java/com/parotha_cloud/ParothaCloudApplicationTests.java
+++ src/test/java/com/parotha_cloud/ParothaCloudApplicationTests.java
@@ -1,13 +1,32 @@
 package com.parotha_cloud;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.hamcrest.Matchers.containsString;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class ParothaCloudApplicationTests {
+
+	@Autowired
+	private MockMvc mockMvc;
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void homePageLoads() throws Exception{
+		mockMvc.perform(get("/api"))
+				.andExpect(status().isOk())
+				.andExpect(view().name("home"))
+				.andExpect(content().string(containsString("Parotha Cloud")));
 	}
 
 }
