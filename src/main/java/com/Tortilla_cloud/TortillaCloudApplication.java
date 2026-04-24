68ee578b790e8f15033c5f51e13053d074cb2f67
Abir-Shaikh <shaikhabir044@gmail.com>
2026-04-24 22:47:56 +0530

    remaining files

---
+++ src/main/java/com/Tortilla_cloud/TortillaCloudApplication.java
@@ -0,0 +1,13 @@
+package com.Tortilla_cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TortillaCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TortillaCloudApplication.class, args);
+	}
+
+}
