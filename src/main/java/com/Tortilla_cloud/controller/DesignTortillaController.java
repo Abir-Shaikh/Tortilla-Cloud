9390312f240a11f0f5a5729bf1b2d34ab4237ce3
Abir-Shaikh <shaikhabir044@gmail.com>
2026-04-25 22:48:16 +0530

    controller logic added

--- src/main/java/com/Tortilla_cloud/controller/DesignTortillaController.java
+++ src/main/java/com/Tortilla_cloud/controller/DesignTortillaController.java
@@ -3,10 +3,13 @@ package com.Tortilla_cloud.controller;
 import com.Tortilla_cloud.model.Ingredient;
 import com.Tortilla_cloud.model.Tortilla;
 import com.Tortilla_cloud.model.Type;
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -17,41 +20,51 @@ import java.util.List;
 @Controller
 @RequestMapping("/design")
 public class DesignTortillaController {
-    @GetMapping
-    public String showDesignForm(Model model) {
-        List<Ingredient> ingredients = Arrays.asList(
-                new Ingredient("FLTO", "Flour Tortilla", Type.WRAP),
-                new Ingredient("COTO", "Corn Tortilla", Type.WRAP),
-                new Ingredient("WHTO", "Whole Wheat Tortilla", Type.WRAP),
+    private static final List<Ingredient> INGREDIENTS = Arrays.asList(
+            new Ingredient("FLTO", "Flour Tortilla", Type.WRAP),
+            new Ingredient("COTO", "Corn Tortilla", Type.WRAP),
+            new Ingredient("WHTO", "Whole Wheat Tortilla", Type.WRAP),
 
-                new Ingredient("SMCH", "Smoked Chicken", Type.PROTEIN),
-                new Ingredient("CARN", "Carnitas", Type.PROTEIN),
-                new Ingredient("SLST", "Sliced Steak", Type.PROTEIN),
+            new Ingredient("SMCH", "Smoked Chicken", Type.PROTEIN),
+            new Ingredient("CARN", "Carnitas", Type.PROTEIN),
+            new Ingredient("SLST", "Sliced Steak", Type.PROTEIN),
 
-                new Ingredient("TMTO", "Diced Tomato", Type.VEGGIES),
-                new Ingredient("LETT", "Lettuce", Type.VEGGIES),
-                new Ingredient("SWPO", "Sweet Potato", Type.VEGGIES),
-                new Ingredient("HUMM", "Hummus", Type.VEGGIES),
+            new Ingredient("TMTO", "Diced Tomato", Type.VEGGIES),
+            new Ingredient("LETT", "Lettuce", Type.VEGGIES),
+            new Ingredient("SWPO", "Sweet Potato", Type.VEGGIES),
+            new Ingredient("HUMM", "Hummus", Type.VEGGIES),
 
-                new Ingredient("CHED", "Cheddar", Type.CHEESE),
-                new Ingredient("JACK", "Monterrey Jack", Type.CHEESE),
+            new Ingredient("CHED", "Cheddar", Type.CHEESE),
+            new Ingredient("JACK", "Monterrey Jack", Type.CHEESE),
 
-                new Ingredient("SLSA", "Salsa", Type.SAUCE),
-                new Ingredient("MAYO", "Mayonnaise", Type.SAUCE)
+            new Ingredient("SLSA", "Salsa", Type.SAUCE),
+            new Ingredient("MAYO", "Mayonnaise", Type.SAUCE)
+    );
 
-        );
-        Type[] types = Type.values();
 
-        for (Type type : types) {
-            model.addAttribute(type.name().toLowerCase(),
-                    filterByType(ingredients, type));
+    @ModelAttribute("design")
+    public Tortilla design() {
+        return new Tortilla();
+    }
+
+
+    @ModelAttribute
+    public void addIngredientsToModel(Model model) {
+        for (Type type : Type.values()) {
+            model.addAttribute(type.name().toLowerCase(), filterByType(INGREDIENTS, type));
         }
-        model.addAttribute("design", new Tortilla());
+    }
+
+    @GetMapping
+    public String showDesignForm() {
         return "design";
     }
 
     @PostMapping
-    public String processDesign(Tortilla design) {
+    public String processDesign(@Valid @ModelAttribute("design") Tortilla design, Errors errors) {
+        if (errors.hasErrors()) {
+            return "design";
+        }
         log.info("Processing design: {}", design);
         return "redirect:/orders/current";
     }
---
+++ src/main/java/com/Tortilla_cloud/controller/OrdersCurrentController.java
@@ -0,0 +1,32 @@
+package com.Tortilla_cloud.controller;
+
+import com.Tortilla_cloud.model.Order;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+@RequestMapping({"/orders", "/order"})
+public class OrdersCurrentController {
+
+    @GetMapping("/current")
+    public String orderForm(Model model){
+        model.addAttribute("order" , new Order());
+        return "orderForm";
+    }
+
+    @PostMapping
+    public String orderSubmission(@Valid Order order , Errors errors){
+        if (errors.hasErrors()) {
+            return "orderForm";
+        }
+        log.info("Order Submiited : " + order);
+        return "redirect:/";
+    }
+}
