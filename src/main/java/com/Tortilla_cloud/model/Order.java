e8feffe2d375d8be7c9b8a76da675192c45a89ef
Abir-Shaikh <shaikhabir044@gmail.com>
2026-04-25 22:55:24 +0530

    model logic added

--- src/main/java/com/Tortilla_cloud/model/Order.java
+++ src/main/java/com/Tortilla_cloud/model/Order.java
@@ -1,15 +1,38 @@
 package com.Tortilla_cloud.model;
 
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
+import org.hibernate.validator.constraints.CreditCardNumber;
 
 @Data
 public class Order {
+    @NotBlank(message = "Name is required")
     private String name;
+
+    @NotBlank(message = "Street is required")
     private String street;
+
+    @NotBlank(message = "City is required")
     private String city;
+
+    @NotBlank(message = "State is required")
     private String state;
+
+    @NotBlank(message = "Zip code is required")
     private String zip;
+
+    @NotBlank(message = "Card number is required")
+    @CreditCardNumber(message = "Not a valid credit card number")
     private String ccNumber;
+
+    @NotBlank(message = "Expiry is required")
+    @Pattern(regexp="^(0[1-9]|1[0-2])([\\/])([1-9][0-9])$",
+            message="Must be formatted MM/YY")
     private String ccExpiration;
+
+    @NotBlank(message = "CVV is required")
+    @Digits(integer=3, fraction=0, message="Invalid CVV")
     private String ccCVV;
 }
--- src/main/java/com/Tortilla_cloud/model/Tortilla.java
+++ src/main/java/com/Tortilla_cloud/model/Tortilla.java
@@ -1,15 +1,20 @@
 package com.Tortilla_cloud.model;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 import lombok.Data;
-import lombok.NoArgsConstructor;
+
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Data
-@NoArgsConstructor
 public class Tortilla {
+
+    @NotBlank(message = "Name must be at least 3 characters long")
+    @Size(min = 3, message = "Name must be at least 3 characters long")
     private String name;
+    @Size(min = 1 , message = "You must choose atleast one ingredient ")
     private List<String> ingredients = new ArrayList<>();
 }
 
