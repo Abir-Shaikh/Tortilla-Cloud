ece10f1604f22e010eafcfae3172062c5f590587
Abir-Shaikh <shaikhabir044@gmail.com>
2026-04-27 21:54:35 +0530

    security config added for rbac

--- src/main/java/com/Tortilla_cloud/security/SecurityConfig.java
+++ src/main/java/com/Tortilla_cloud/security/SecurityConfig.java
@@ -1,5 +1,34 @@
 package com.Tortilla_cloud.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig{
 
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/" , "/register").permitAll()
+                        .requestMatchers("/design" , "/orders").authenticated()
+                        .anyRequest().authenticated())
+
+                .logout(logout -> logout
+                        .logoutSuccessUrl("/")
+                        .permitAll())
+
+
+                .formLogin(
+                        login -> login
+                                .loginPage("/login")
+                                .defaultSuccessUrl("/design" , true)
+                                .permitAll()
+                );
+        return http.build();
+    }
 }
