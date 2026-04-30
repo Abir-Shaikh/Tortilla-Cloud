eb0e6859c574da8c79247129b048615779617aea
Abir-Shaikh <shaikhabir044@gmail.com>
2026-04-30 22:52:32 +0530

    pagination added another section for my orders created

--- src/main/java/com/Tortilla_cloud/controller/OrdersCurrentController.java
+++ src/main/java/com/Tortilla_cloud/controller/OrdersCurrentController.java
@@ -1,10 +1,13 @@
 package com.Tortilla_cloud.controller;
 
+import com.Tortilla_cloud.configuration.OrderProps;
 import com.Tortilla_cloud.model.Order;
 import com.Tortilla_cloud.model.User;
 import com.Tortilla_cloud.repository.OrderRepository;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,15 +17,28 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.support.SessionStatus;
 
+import java.util.List;
+
 @Slf4j
 @Controller
-@RequestMapping({"/orders", "/order"})
+@RequestMapping("/orders")
 public class OrdersCurrentController {
 
     private final OrderRepository orderRepository;
+    private final OrderProps orderProps;
 
-    public OrdersCurrentController(OrderRepository orderRepository) {
+    public OrdersCurrentController(OrderRepository orderRepository, OrderProps orderProps) {
         this.orderRepository = orderRepository;
+        this.orderProps = orderProps;
+    }
+
+    @GetMapping
+    public String OrderForUsers(Model model , @AuthenticationPrincipal User user){
+        Pageable pageable = PageRequest.of(0 , orderProps.getPageSize());
+        List<Order> orders = orderRepository.findByUser(user , pageable);
+        model.addAttribute("orders" , orders);
+        log.info("Fetched {} orders for user: {}", orders.size(), user.getUsername());
+        return "orderList";
     }
 
     @GetMapping("/current")
