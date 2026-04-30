02b986b5cdf0f47857025e4a65f1c73775fe23e5
Abir-Shaikh <shaikhabir044@gmail.com>
2026-04-30 22:53:24 +0530

    added pagination for users

--- src/main/java/com/Tortilla_cloud/repository/OrderRepository.java
+++ src/main/java/com/Tortilla_cloud/repository/OrderRepository.java
@@ -1,16 +1,20 @@
 package com.Tortilla_cloud.repository;
 
 import com.Tortilla_cloud.model.Order;
+import com.Tortilla_cloud.model.User;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.Date;
 import java.util.List;
 
-public interface OrderRepository extends CrudRepository<Order , Long> {
+public interface OrderRepository extends CrudRepository<Order , Long> , PagingAndSortingRepository<Order , Long> {
+
+    List<Order> findByUser(User user, Pageable pageable);
 
     List<Order> findByZip(String zip);
 
     List<Order> findByCity(String city);
 
-    List<Order> findByPlacedAtBetween(Date start, Date end);
 }
