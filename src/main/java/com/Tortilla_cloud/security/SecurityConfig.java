43c1ac4e1e4cea0b61a03d3ed270773e7aa2add0
Abir-Shaikh <shaikhabir044@gmail.com>
2026-05-03 19:02:11 +0530

    permitted some endpoints removed the extra links causing error and filter and web chains are fixed

--- src/main/java/com/Tortilla_cloud/security/SecurityConfig.java
+++ src/main/java/com/Tortilla_cloud/security/SecurityConfig.java
@@ -2,13 +2,13 @@ package com.Tortilla_cloud.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
 import static org.springframework.http.HttpStatus.UNAUTHORIZED;
@@ -23,12 +23,13 @@ public class SecurityConfig{
     }
 
     @Bean
+    @Order(1)
     SecurityFilterChain apiFilterChain(HttpSecurity http) throws Exception {
-        http.securityMatcher(new AntPathRequestMatcher("/api/**"))
+        http.securityMatcher("/api/**")
                 .authorizeHttpRequests(auth -> auth
                         // Public APIs
-                        .requestMatchers("/api/tortillas", "/api/tortillas/**").permitAll()
-                        .requestMatchers("/api/ingredients", "/api/ingredients/**").permitAll()
+                        .requestMatchers("/api/tortillas/**").permitAll()
+                        .requestMatchers("/api/ingredients/**").permitAll()
                         .anyRequest().authenticated())
                 .exceptionHandling(eh -> eh.authenticationEntryPoint(new HttpStatusEntryPoint(UNAUTHORIZED)))
                 .httpBasic(basic -> {})
@@ -39,8 +40,11 @@ public class SecurityConfig{
     }
 
     @Bean
+    @Order(2)
     SecurityFilterChain webFilterChain(HttpSecurity http) throws Exception{
-        http.authorizeHttpRequests(auth -> auth
+        http
+                .securityMatcher(request -> !request.getRequestURI().startsWith("/api/"))
+                .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/images/**", "/menu/**", "/buttons/**", "/css/**", "/js/**", "/webjars/**", "/favicon.ico").permitAll()
                         .requestMatchers("/h2-console/**").permitAll()
                         .requestMatchers("/" , "/login" , "/register").permitAll()
