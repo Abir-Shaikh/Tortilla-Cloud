5be8c9e62c397198fbccbae94c8187c82c522398
Abir-Shaikh <shaikhabir044@gmail.com>
2026-05-03 18:59:49 +0530

    Added Page instead of list cuz page contains data , total pages, current page number and all

--- src/main/java/com/Tortilla_cloud/repository/OrderRepository.java
+++ src/main/java/com/Tortilla_cloud/repository/OrderRepository.java
@@ -2,6 +2,7 @@ package com.Tortilla_cloud.repository;
 
 import com.Tortilla_cloud.model.Order;
 import com.Tortilla_cloud.model.User;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
@@ -11,7 +12,7 @@ import java.util.List;
 
 public interface OrderRepository extends CrudRepository<Order , Long> , PagingAndSortingRepository<Order , Long> {
 
-    List<Order> findByUser(User user, Pageable pageable);
+    Page<Order> findByUser(User user, Pageable pageable);
 
     List<Order> findByZip(String zip);
 
--- src/main/java/com/Tortilla_cloud/repository/TortillaRepository.java
+++ src/main/java/com/Tortilla_cloud/repository/TortillaRepository.java
@@ -1,6 +1,8 @@
 package com.Tortilla_cloud.repository;
 
 import com.Tortilla_cloud.model.Tortilla;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.Date;
@@ -9,4 +11,5 @@ import java.util.List;
 public interface TortillaRepository extends CrudRepository<Tortilla , Long> {
 
     List<Tortilla> findByCreatedAtAfter(Date date);
+    Page<Tortilla> findAll(Pageable pageable);
 }
