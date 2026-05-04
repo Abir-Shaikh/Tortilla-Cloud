3fe96099c6c7033d607c1f30a981c36343c640a5
Abir-Shaikh <shaikhabir044@gmail.com>
2026-05-04 21:38:32 +0530

    Rest template configured

---
+++ src/main/java/com/Tortilla_cloud/controller/testingAPIController/IngredientClientController.java
@@ -0,0 +1,4 @@
+package com.Tortilla_cloud.controller.testingAPIController;
+
+public class IngredientClientController {
+}
---
+++ src/main/java/com/Tortilla_cloud/service/IngredientServiceClient.java
@@ -0,0 +1,4 @@
+package com.Tortilla_cloud.service;
+
+public class IngredientServiceClient {
+}
