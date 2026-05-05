f9cee779e3a75071f2addd022666569176a0eaed
Abir-Shaikh <shaikhabir044@gmail.com>
2026-05-05 23:49:48 +0530

    created dto for orderMessage

---
+++ src/main/java/com/Tortilla_cloud/DTO/OrderMessage.java
@@ -0,0 +1,33 @@
+package com.Tortilla_cloud.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderMessage implements Serializable {
+
+    private Long orderId;
+    private String customerName;
+    private String customerEmail;
+    private String deliveryCity;
+    private String deliveryState;
+    private String message;
+    private long timestamp;
+
+    public static OrderMessage from(Long orderId , String customerName , String city , String state){
+        return new OrderMessage(
+                orderId,
+                customerName,
+                customerName.toLowerCase().replace("" , ".") + "@tortillacloud.com",
+                city,
+                state,
+                "New Order placed",
+                System.currentTimeMillis()
+        );
+    }
+}
