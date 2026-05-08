11509c51bb345d48f0a6fa96c5d52d823b972f36
Abir-Shaikh <shaikhabir044@gmail.com>
2026-05-08 22:59:07 +0530

    rabbitmq configured

--- src/main/java/com/Tortilla_cloud/configuration/RabbitConfig.java
+++ src/main/java/com/Tortilla_cloud/configuration/RabbitConfig.java
@@ -1,6 +1,11 @@
 package com.Tortilla_cloud.configuration;
 
 import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Bean;
 
@@ -33,5 +38,30 @@ public class RabbitConfig {
         return BindingBuilder.bind(orderQueue)
                 .to(orderExchange)
                 .with(ORDER_ROUTING_KEY);
+    }
+
+    // Use JSON instead of Java serialization (avoids deserialization allow-list errors and is safer).
+    @Bean
+    public MessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory, MessageConverter messageConverter) {
+        RabbitTemplate template = new RabbitTemplate(connectionFactory);
+        template.setMessageConverter(messageConverter);
+        return template;
+    }
+
+    // Ensures @RabbitListener uses the same JSON converter.
+    @Bean
+    public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(
+            ConnectionFactory connectionFactory,
+            MessageConverter messageConverter
+    ) {
+        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
+        factory.setMessageConverter(messageConverter);
+        return factory;
     }
 }
